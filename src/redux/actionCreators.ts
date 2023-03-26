1b4ad6028976c97b8e4f2ab8344b0466388e5368
Krystian Guz <krystian.guz@outlook.com>
2023-03-26 23:50:57 +0200

    Update actionCreators.ts

--- src/redux/actionCreators.ts
+++ src/redux/actionCreators.ts
@@ -1,16 +1,10 @@
+import { act } from "@testing-library/react";
 import * as actionTypes from "./actionTypes";
 
-export function setList(view: IView) {
-  const action: viewAction = {
-    type: actionTypes.SET_LIST,
-    view,
-  };
-}
 export function setView(view: IView) {
   const action: viewAction = {
     type: actionTypes.SET_VIEW,
     view,
   };
-
   return action;
 }
